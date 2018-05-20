013bdf8417b9f334b35834858ea617f136bb7786
Davydov Roman <warlight@mail.ru>
2018-05-20 20:50:35 +0300

    migrations for tables cats, breeds, houses

---
+++ database/migrations/2018_05_13_190241_create_cats_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cats', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('breed_id')->nullable();
+            $table->integer('age');
+            $table->integer('house_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cats');
+    }
+}
