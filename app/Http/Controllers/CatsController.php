2db9aace2192e06d0e54e667209bdbdf2d8c220d
Davydov Roman <warlight@mail.ru>
2018-05-20 22:56:45 +0300

    empty controller - structure

---
+++ app/Http/Controllers/CatsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class CatsController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function breeds()
+    {
+
+    }
+
+    public function statistics()
+    {
+        
+    }
+}
